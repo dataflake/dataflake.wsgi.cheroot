c0b0ff7972cbc08022945decb14b50a4d6ba9c9a
Jens Vagelpohl <jens@netz.ooo>
2019-04-20 17:49:24 -0500

    - support all legal arguments for the cheroot server creation

--- dataflake/wsgi/cheroot/__init__.py
+++ dataflake/wsgi/cheroot/__init__.py
@@ -35,10 +35,17 @@ class LoggingWSGIServer(wsgi.Server):
         CHEROOT_ERROR_LOGGER.log(level, msg, exc_info=traceback)
 
 
+def _makeBool(value):
+    """ Helper to make boolean out of a .ini value """
+    if value is None or value.lower() in ('off', 'false', '0'):
+        return False
+    return True
+
+
 def serve_paste(app, global_conf, **kw):
     """ A handler for PasteDeploy-compatible runners.
 
-    Sample .ini configuration:
+    Sample minimal .ini configuration:
 
       [server:main]
       use = egg:dataflake.wsgi.cheroot#main
@@ -49,11 +56,22 @@ def serve_paste(app, global_conf, **kw):
     interfaces.
     If you don't provide a port value, the server will listen on port 8080.
     """
-    host = kw.get('host', '0.0.0.0')
-    port = kw.get('port', '8080')
+    host = kw.pop('host', '0.0.0.0')
+    port = kw.pop('port', '8080')
     address = (host, int(port))
 
-    server = LoggingWSGIServer(address, app)
+    for key in ('numthreads', 'max', 'request_queue_size', 'timeout',
+                'shutdown_timeout', 'accepted_queue_size',
+                'accepted_queue_timeout'):
+        # fix up arguments that should be integers
+        if key in kw:
+            kw[key] = int(kw[key])
+
+    for key in ('peercreds_enabled', 'peercreds_resolve_enabled'):
+        if key in kw:
+            kw[key] = _makeBool(kw[key])
+
+    server = LoggingWSGIServer(address, app, **kw)
     try:
         server.start()
     except KeyboardInterrupt:
