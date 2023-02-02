811f1d3fa1be1dbf535874bd7beeae8108bcd476
Jens Vagelpohl <jens@plyp.com>
2023-02-02 11:21:16 +0100

    - tickle GHA

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from setuptools import setup
 
 
 NAME = 'dataflake.wsgi.cheroot'
-URL = 'https://github.com/dataflake/%s' % NAME
+URL = f'https://github.com/dataflake/{NAME}'
 
 
 def read(*rnames):
@@ -32,7 +32,7 @@ setup(
     url=URL,
     project_urls={
         'Documentation': 'https://dataflakewsgicheroot.readthedocs.io',
-        'Issue Tracker': '%s/issues' % URL,
+        'Issue Tracker': f'{URL}/issues',
         'Sources': URL,
     },
     license='ZPL 2.1',
