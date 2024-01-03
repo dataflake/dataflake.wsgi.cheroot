7919ef8660a2496cd0a84c282c020763e63fde13
Jens Vagelpohl <jens@plyp.com>
2024-01-03 17:14:12 +0100

    - add support for Python 3.12 and use latest meta/config

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup(
     name='dataflake.wsgi.cheroot',
-    version='2.1.dev0',
+    version='2.1',
     url='https://github.com/dataflake/dataflake.wsgi.cheroot',
     project_urls={
         'Documentation': 'https://dataflakewsgicheroot.readthedocs.io',
